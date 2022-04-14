b018d24ee5b787258dcca00069dc976fa20eaece
Jim Falls <jimf@superiorcsgroup.com>
2022-04-14 16:46:47 +0530

    add init

---
+++ src/controllers/books.controller.js
@@ -0,0 +1,52 @@
+/**
+ * User controller
+ */
+const Bookkeep = require("../models/Bookeep.model");
+
+module.exports = {
+  create: async function create(req, res) {
+    let data = req.body;
+    try {
+      const book = await Bookkeep.create({
+        name: data.name,
+        author: data.author,
+        link: data.link,
+      });
+      return res.status(200).json(book.toJSON());
+    } catch (e) {
+      return res.status(500).json({ message: "error creating data!", e });
+    }
+  },
+  createNewbook: async function create(payload, res) {
+    let data = payload;
+    try {
+      const book = await Bookkeep.create({
+        name: data.name,
+        author: data.author,
+        link: data.link,
+      });
+      if (book) {
+        res.redirect("/home");
+      }
+    } catch (e) {
+      console.log("e", e);
+      // res.status(500).json({ message: "error creating data!", e });
+    }
+  },
+  Deleteabook: async function createDelete(payload, res) {
+    try {
+      let data = payload;
+      const book = await Bookkeep.deleteOne({
+        _id: data.id,
+      });
+      console.log("data --->>>", data);
+      // res.redirect("/home");
+      return res.status(200).json(payload);
+      // if (book) {
+      // }
+    } catch (e) {
+      console.log("e", e);
+      // res.status(500).json({ message: "error creating data!", e });
+    }
+  },
+};
